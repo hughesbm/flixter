3162fa9d69ea1309c638b598788bac2d36b6ef64
Brenton Hughes <brenton.hughes@gmail.com>
2016-08-20 01:35:28 +0000

    Added video uploading functionality

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,8 @@
 class Lesson < ApplicationRecord
   belongs_to :section
 
+  mount_uploader :video, VideoUploader
+
   validates :title, presence: true, length: { minimum: 3, maximum: 30 }
   validates :subtitle, presence: true, length: { minimum: 3, maximum: 30 }
 end
---
+++ app/uploaders/video_uploader.rb
@@ -0,0 +1,57 @@
+# encoding: utf-8
+
+class VideoUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  # storage :file
+  # storage :fog
+  storage :aws
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
+
+  # Allow only mp4 files
+  def extension_white_list
+    %w(mp4)
+  end
+
+  # Override the filename of the uploaded files w/ uuid:
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  private
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+
+end
