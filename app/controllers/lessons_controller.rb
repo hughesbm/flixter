e003c72f65d93bb42c2af9fe3707582b9ccdf207
Brenton Hughes <brenton.hughes@gmail.com>
2016-08-28 21:04:32 +0000

    Added user permissions to lessons, logging in now redirects to last page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
   def show
   end
@@ -9,4 +11,12 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_authorized_for_current_section
+    unless current_user && (current_user.enrolled_in?(current_lesson.section.course) || current_lesson.section.course.user == current_user)
+      flash[:alert] = "Lessons are only available to enrolled students. Click the \"Enroll in Course\" button below."
+      redirect_to course_path(current_lesson.section.course)
+    end
+  end
+
 end
