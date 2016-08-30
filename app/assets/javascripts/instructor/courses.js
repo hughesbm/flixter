0400e9e82e97913f9c78ca70863fd82b8f2c277f
Brenton Hughes <brenton.hughes@gmail.com>
2016-08-30 18:05:52 +0000

    DRYed up lesson/section sorting javascript

--- app/assets/javascripts/instructor/courses.js
+++ app/assets/javascripts/instructor/courses.js
@@ -1,26 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //
-// Need to implement ES6 transpiling in order to DRY this up (ES6 allows setting object keys dynamically)
 $(function() {
-  $('.lessons').sortable({
-    update: function(event, ui) {
-      $.ajax({
-        type: 'PUT',
-        url: ui.item.data('update-url'),
-        dataType: 'json',
-        data: { lesson : { row_order_position : ui.item.index() } }
-      });
-    }
-  });
-  $('.sections').sortable({
-    update: function(event, ui) {
-      $.ajax({
-        type: 'PUT',
-        url: ui.item.data('update-url'),
-        dataType: 'json',
-        data: { section : { row_order_position : ui.item.index() } }
-      });
-    }
-  });
+  const sortableObjects = ["lessons", "sections"]
+  let ajaxParams = {
+    type: 'PUT',
+    dataType: 'json'
+  };
+  for (let sortObject of sortableObjects) {
+    $(document.getElementsByClassName(sortObject)).sortable({
+      update: function(event, ui) {
+        ajaxParams.url = ui.item.data('update-url');
+        if (sortObject == "lessons") {
+          ajaxParams.data = { lesson : { row_order_position : ui.item.index() } };
+        } else if (sortObject == "sections") {
+          ajaxParams.data = { section : { row_order_position : ui.item.index() } };
+        }
+        $.ajax(ajaxParams);
+      }
+    });
+  }
 });
