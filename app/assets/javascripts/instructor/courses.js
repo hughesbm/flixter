f61fb23d55e9d53e2c5f580db7013dfe8fd013bd
Brenton Hughes <brenton.hughes@gmail.com>
2016-08-31 23:17:32 +0000

    New sections and lessons are now created through modals

--- app/assets/javascripts/instructor/courses.js
+++ app/assets/javascripts/instructor/courses.js
@@ -1,14 +1,22 @@
 $(function() {
+  // Set icons for accordion
   const sectionIcons = {
     header: "glyphicon glyphicon-menu-left",
     activeHeader: "glyphicon glyphicon-menu-down"
   };
+  // Sets proper url for lesson creation
+  $('.new-lesson-card').click(function(){
+    var lessonUrl = $( this ).data('lesson-url');
+    $('#newLessonForm').attr('action', lessonUrl);
+  });
+  // Accordions sections
   $('.sections').accordion({
     header: "> div > div.section-item-header",
     heightStyle: "content",
     icons: sectionIcons,
     collapsible: true
   });
+  // Make lessons and sections sortable, put new order in db
   $('.lessons').sortable({
     update: function(event, ui) {
       $.ajax({
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-  resources :lessons, only: [:show]
+  resources :lessons, only: :show
   namespace :instructor do
     resources :lessons, only: :update
     resources :sections, only: :update do
-      resource :lessons, only: [:new, :create]
+      resource :lessons, only: :create
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: :create
     end
   end
 end
