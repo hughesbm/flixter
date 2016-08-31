eb812d5abf03263f279824895b672077ef816132
Brenton Hughes <brenton.hughes@gmail.com>
2016-08-31 10:40:07 +0000

    More tweaks to appease the precompiler

--- app/assets/javascripts/instructor/courses.js
+++ app/assets/javascripts/instructor/courses.js
@@ -1,12 +1,7 @@
 $(function() {
-  const sortableObjects = ["lessons", "sections"];
   const sectionIcons = {
     header: "glyphicon glyphicon-menu-left",
     activeHeader: "glyphicon glyphicon-menu-down"
-  };
-  var ajaxParams = {
-    type: 'PUT',
-    dataType: 'json'
   };
   $('.sections').accordion({
     header: "> div > div.section-item-header",
@@ -14,18 +9,24 @@ $(function() {
     icons: sectionIcons,
     collapsible: true
   });
-  for (let sortObject of sortableObjects) {
-    $(document.getElementsByClassName(sortObject))
-    .sortable({
-      update: function(event, ui) {
-        ajaxParams.url = ui.item.data('update-url');
-        if (sortObject == "lessons") {
-          ajaxParams.data = { lesson : { row_order_position : ui.item.index() } };
-        } else if (sortObject == "sections") {
-          ajaxParams.data = { section : { row_order_position : ui.item.index() } };
-        }
-        $.ajax(ajaxParams);
-      }
-    });
-  }
+  $('.lessons').sortable({
+    update: function(event, ui) {
+      $.ajax({
+        type: 'PUT',
+        dataType: 'json',
+        url: ui.item.data('update-url'),
+        data: { lesson : { row_order_position : ui.item.index() } }
+      });
+    }
+  });
+  $('.sections').sortable({
+    update: function(event, ui) {
+      $.ajax({
+        type: 'PUT',
+        dataType: 'json',
+        url: ui.item.data('update-url'),
+        data: { section : { row_order_position : ui.item.index() } }
+      });
+    }
+  });
 });
